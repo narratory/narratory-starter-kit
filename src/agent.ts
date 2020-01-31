9bec4adaa305d0b61648592e87bf56b42b793d81
luddilo <ludvig@furhatrobotics.com>
2020-01-31 08:19:14 +0100

    Updated to 0.5.0 Narratory version and added narratory_credentials.json file

--- src/agent.ts
+++ src/agent.ts
@@ -8,7 +8,7 @@ const agent: Agent = {
     narrative, // See the file narrative.ts
     userInitiatives, // See the file userInitiatives.ts
     bridges: ["So", "Where were we", "Now"],
-    narratoryKey: "ENTER-NARRATORY-KEY-HERE", // Enter your Narratory key here, sign up att narratory.io if you don't have one
+    narratoryKey: require("../narratory_credentials.json").narratoryKey, // Populate this file with your Narratory key. Sign up att narratory.io if you don't have one!
     googleCredentials: require("../google_credentials.json") // Populate this file, or change the link to your existing credentials file. Check the README.md for how to create it.
 }
 
