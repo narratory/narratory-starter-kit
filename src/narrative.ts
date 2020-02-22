3b1283b2109e76dbcdc34a8d64500fe491aa03ac
luddilo <ludvig@furhatrobotics.com>
2020-02-23 00:05:28 +0100

    Linking to docs for getting-started guide. Added prettier-config and formatted all files. Version bump 0.6.6

--- src/narrative.ts
+++ src/narrative.ts
@@ -8,40 +8,57 @@ import * as intents from "./nlu"
 const greeting = ["Hi there", "Greetings", "hello"]
 
 const metBefore: BotTurn = {
-    say: "have we met before?",
-    user: [
-        { intent: ["Yes", "Indeed", "Absolutely", "we have", "I think so"], bot: {
-            say: "Oh really, do you remember when?", user: [
-                { intent: ANYTHING, bot: ["Interesting", "I see"] }
-            ]}
-        },
-        { intent: ["No", "I don't think so", "I have no idea", "I don't know"], bot: "Well, it is a pleasure to talk to you!" }
-    ]
+  say: "have we met before?",
+  user: [
+    {
+      intent: ["Yes", "Indeed", "Absolutely", "we have", "I think so"],
+      bot: {
+        say: "Oh really, do you remember when?",
+        user: [{ intent: ANYTHING, bot: ["Interesting", "I see"] }]
+      }
+    },
+    {
+      intent: ["No", "I don't think so", "I have no idea", "I don't know"],
+      bot: "Well, it is a pleasure to talk to you!"
+    }
+  ]
 }
 
 const favVirtualAssistant: BotTurn = {
-    say: ["Which is your favorite virtual assistant?", "Who is your favorite virtual assistant?"],
-    user: [
-        { intent: intents.favAssistant, bot: `Oh, I love _myFavAssistant` },
-        { intent: ["What is a virtual assistant", "what do you mean"], bot: {
-            say: "For example, Alexa from Amazon, Google Assistant and of course Siri from Apple. Do you have a favorite?",
-            repair: true
-        }},
-        { intent: ["No", "I don't have a favorite", "No one", "none", "They all stink"], bot: "Oh, well. That's okay. Maybe you'll like me more!" }
-    ]
+  say: ["Which is your favorite virtual assistant?", "Who is your favorite virtual assistant?"],
+  user: [
+    { intent: intents.favAssistant, bot: `Oh, I love _myFavAssistant` },
+    {
+      intent: ["What is a virtual assistant", "what do you mean"],
+      bot: {
+        say: "For example, Alexa from Amazon, Google Assistant and of course Siri from Apple. Do you have a favorite?",
+        repair: true
+      }
+    },
+    {
+      intent: ["No", "I don't have a favorite", "No one", "none", "They all stink"],
+      bot: "Oh, well. That's okay. Maybe you'll like me more!"
+    }
+  ]
 }
 
 const builtAssistantApp: BotTurn = {
-    cond: { myFavAssistant: true },
-    say: `Have you tried building an app for _myFavAssistant?`,
-    user: [
-        { intent: ["yes", "I have", "yep", "of course", "absolutely"], bot: "Oh, that is fantastic. I'm glad you're about to try building one with me now!" },
-        { intent: ["no", "I have not", "nope", "not yet"], bot: "I see. Well, happy to have you here!" }
-    ]
+  cond: { myFavAssistant: true },
+  say: `Have you tried building an app for _myFavAssistant?`,
+  user: [
+    {
+      intent: ["yes", "I have", "yep", "of course", "absolutely"],
+      bot: "Oh, that is fantastic. I'm glad you're about to try building one with me now!"
+    },
+    {
+      intent: ["no", "I have not", "nope", "not yet"],
+      bot: "I see. Well, happy to have you here!"
+    }
+  ]
 }
 
 const goodbye: BotTurn = {
-    say: ["That's it for now, it was a pleasure to talk to you!"]
+  say: ["That's it for now, it was a pleasure to talk to you!"]
 }
 
 export default [greeting, metBefore, favVirtualAssistant, builtAssistantApp, goodbye]
--- src/nlu.ts
+++ src/nlu.ts
@@ -1,33 +1,29 @@
 import { Entity, Intent } from "narratory"
 
 export const favNumber: Intent = {
-    examples: [
-        "what is your favorite number",
-        "what number do you like best",
-        "what number gets you going"
-    ]
+  examples: ["what is your favorite number", "what number do you like best", "what number gets you going"]
 }
 
 export const virtualAssistant: Entity = {
-    name: "virtualAssistant",
-    enums: [
-        { name: "alexa", alts: ["alexa", "Amazon Alexa", "The amazon one"] },
-        { name: "google home", alts: ["Google home", "Google assistant", "assistant from google"] },
-        { name: "siri" },
-        { name: "Cortana", alts: ["microsoft assistant"] }
-    ]
+  name: "virtualAssistant",
+  enums: [
+    { name: "alexa", alts: ["alexa", "Amazon Alexa", "The amazon one"] },
+    { name: "google home", alts: ["google", "Google assistant", "assistant from google"] },
+    { name: "siri" },
+    { name: "Cortana", alts: ["microsoft assistant"] }
+  ]
 }
 
 export const favAssistant: Intent = {
-    entities: {
-        myFavAssistant: virtualAssistant
-    },
-    examples: [
-        "I love siri",
-        "I talk to alexa at home",
-        "I have a google home",
-        "my computer has cortana",
-        "I like _myFavAssistant",
-        "google assistant on my phone"
-    ]
+  entities: {
+    myFavAssistant: virtualAssistant
+  },
+  examples: [
+    "I love siri",
+    "I talk to alexa at home",
+    "I have a google home",
+    "my computer has cortana",
+    "I like _myFavAssistant",
+    "google assistant on my phone"
+  ]
 }
